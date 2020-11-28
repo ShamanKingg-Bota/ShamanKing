2f56da25adb35f639026f1dfd5b75a0444eaf8d6
Shagirovva <Shagirova.akb@gmail.com>
2020-11-29 01:29:08 +0600

    Описать сущность Episode

---
+++ domain/ShamanKing/Episode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ShamanKing
+{
+    public class Episode
+    {
+        public int Id { get; }//идентификатор, чтобы не путать
+        public string Title { get; }//название эпизода,возвращаемый
+        public Episode(int id, string title)//кажись конструктор?
+        {
+            Id = id;
+            Title = title; //проинициализировать
+        }
+
+    }
+}
