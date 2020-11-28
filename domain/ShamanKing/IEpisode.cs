0656a4be7c48d3bcb83ee34c4de168ce3dc3a1b9
Shagirovva <Shagirova.akb@gmail.com>
2020-11-29 02:51:36 +0600

    Добавить интерфейс репозитория эпизодов

--- domain/ShamanKing/IEpisode.cs
+++ domain/ShamanKing/IEpisode.cs
@@ -6,7 +6,6 @@ namespace ShamanKing
 {
     interface IEpisode
     {
-        Episode[] GetAllTitle(string titlePart);//этот метод будет получать часть названия и 1 или несколько,или ни 1го эпизодов
- 
+        Episode[] GetAllTitle(string titlePart);//этот метод будет получать часть названия, и возращать эпизод
     }
 }
