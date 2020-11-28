d48187fb02c412e643191300a35d02621e25f549
Shagirovva <Shagirova.akb@gmail.com>
2020-11-29 05:47:15 +0600

    add Interface - IEpisodeRepository

---
+++ domain/ShamanKing/IEpisodeRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShamanKing
+{
+    interface IEpisodeRepository
+    {
+        Episode[] GetAllByTitle(string titlePart);//этот метод будет получать часть названия, и возращать эпизод
+    }
+}
