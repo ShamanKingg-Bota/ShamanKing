5b585095fb9e5ecb0e19e05f4a36ae4db92121a2
Shagirovva <Shagirova.akb@gmail.com>
2020-11-29 06:28:14 +0600

    Создать веб-приложение Shaman.Web

---
+++ presentation/Shaman.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Shaman.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
