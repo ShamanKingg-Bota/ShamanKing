95d66ee1c3fbe3f030858b08106649f5409f9c9a
Shagirovva <Shagirova.akb@gmail.com>
2020-11-29 06:13:19 +0600

    добавить реальзацию репозитории эпизодов в памяти

---
+++ infrastructure/Shaman.Memory/EpisodeRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using ShamanKing;
+
+namespace Shaman.Memory
+{
+    public class EpisodeRepository : IEpisodeRepository
+    {
+        private readonly Episode[] episodes = new[]
+        {
+            new Episode(1, "Мальчик, танцующий с призраками"),
+            new Episode(2, " Ожидающий самурай"),
+            new Episode(3,"Другой шаман"),
+        };
+        public Episode[] GetAllByTitle(string titlePart)
+        {
+            return episodes.Where(episode => episode.Title.Contains(titlePart))
+                .ToArray();
+        }
+    }
+}
