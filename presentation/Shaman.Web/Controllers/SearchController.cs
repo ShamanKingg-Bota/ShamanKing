3206522cd5c1889a8e77af723a5bd4ea10900638
Shagirovva <Shagirova.akb@gmail.com>
2020-12-01 19:56:10 +0600

    aa

---
+++ presentation/Shaman.Web/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShamanKing;
+
+namespace Shaman.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IEpisodeRepository episodeRepository;
+        public SearchController(IEpisodeRepository episodeRepository)
+        {
+            this.episodeRepository = episodeRepository;
+        }
+
+        public IActionResult Index(string query)
+        {
+            var episodes = episodeRepository.GetAllByTitle(query);
+            return View(episodes);
+        }
+    }
+}
